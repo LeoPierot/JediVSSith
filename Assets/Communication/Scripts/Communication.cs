5f00c41317c8ab690937e3acd3572f34ba00fdbc
unknown <Administrateur@JMIN-D1717.formation.enjmin.fr>
2018-11-05 18:01:45 +0100

    Add the test code for the phrase recognition

--- Assets/Communication/Scripts/Communication.cs
+++ Assets/Communication/Scripts/Communication.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 
 public class Communication : MonoBehaviour {
 
+	public KeywordRecognizer speachApi = null;
+	public string[] keyword;
+
 	// Use this for initialization
 	void Start () {
-		
+		if(!InitializeCommunication()){
+			return;
+		}
+		Debug.Log("Communication initialized");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	bool InitializeCommunication(){
+		if(!PhraseRecognitionSystem.isSupported){
+			return false;
+		}
+		this.speachApi = new KeywordRecognizer(this.keyword);
+		this.speachApi.OnPhraseRecognized += OnVoiceRecognition;
+		this.speachApi.Start();
+		return true;
+	}
+
+	void OnVoiceRecognition(PhraseRecognizedEventArgs args){
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        stringBuilder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        stringBuilder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(stringBuilder.ToString());
 	}
 }
