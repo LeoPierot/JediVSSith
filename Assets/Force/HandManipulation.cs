0118e07f679ebc695ae1c24031c792a39224ed59
Léo Piérot <leo.fpierot@gmail.com>
2018-11-06 10:50:08 +0100

    manipulation
    début manipulation

---
+++ Assets/Force/HandManipulation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HandManipulation : MonoBehaviour
+{
+    Vector3 offset = new Vector3(0.0f, 0.0f, 0.0f);
+
+	void Start ()
+    {
+
+    }
+	
+	void Update ()
+    {
+        if (SteamVR_Input._default.inActions.GrabGrip.GetStateDown(SteamVR_Input_Sources.LeftHand))
+        {
+            //GameObject toCatch = null;
+            //toCatch.transform.position = transform.position + offset;
+            Debug.Log("Fuck off");
+        }
+    }
+}
