3039872557e37cc6ebcfa873a94ad8e905b56c44
unknown <Administrateur@JMIN-D1727.formation.enjmin.fr>
2018-11-14 17:27:55 +0100

    Collider sabre et repoussage dobjet

---
+++ Assets/Sabre/Scripts/SaberBlade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaberBlade : MonoBehaviour {
+
+    public float BladeStrength = 10f;
+    private Rigidbody m_Rb = null;
+
+    private Vector3 m_LastPosition = Vector3.zero;
+    private Vector3 m_Direction = Vector3.zero;
+
+	void Start ()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+        m_LastPosition = transform.position;
+    }
+	
+    void Update ()
+    {
+        m_Direction = transform.position - m_LastPosition;
+        m_LastPosition = transform.position;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(m_Direction.normalized);
+        collider.gameObject.GetComponent<Rigidbody>().velocity = m_Direction.normalized * BladeStrength;
+        if (collider.CompareTag("damage"))
+        {
+            
+        }
+    }
+}
