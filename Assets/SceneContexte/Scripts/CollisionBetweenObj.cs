cd64671e0653e31153b72d60a6018def3b85277a
DESKTOP-Q7DIQ2E\sulo_ <sulo_le_turc@hotmail.fr>
2018-11-14 11:18:18 +0100

    (feat):ajout le son de collision + activation du son lors d'une collision entre deux objets

---
+++ Assets/SceneContexte/Scripts/CollisionBetweenObj.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class qui active le son de collision lorsque l'objet rencontre un autre
+/// </summary>
+public class CollisionBetweenObj : MonoBehaviour
+{
+
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Fetch the AudioSource from the GameObjec
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Zone_Fight")
+        {
+            source.Play();
+            Debug.Log("COLLISION");
+        }
+        else
+        {
+            source.Play();
+            Debug.Log("COLLISION");
+        }
+    }
+}
