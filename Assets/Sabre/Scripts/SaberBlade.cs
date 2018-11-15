3cf34195ab4d43a9088da5e8fb31f028d9da75e2
unknown <Administrateur@JMIN-D1727.formation.enjmin.fr>
2018-11-15 11:25:51 +0100

    Add haptic feedback on collide

--- Assets/Sabre/Scripts/SaberBlade.cs
+++ Assets/Sabre/Scripts/SaberBlade.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class SaberBlade : MonoBehaviour {
 
     public float BladeStrength = 10f;
     private Rigidbody m_Rb = null;
-
+    //[SteamVR_DefaultAction("Haptic")]
+    public SteamVR_Action_Vibration hapticSignal;
+   // public SteamVR_Input_Sources controller;
     private Vector3 m_LastPosition = Vector3.zero;
     private Vector3 m_Direction = Vector3.zero;
 
-	void Start ()
+    public float timeBeforeActivation = 0.0f;
+    public float Duration = 0.1f;
+    public float Frequency = 1.0f;
+    public float Amplitude = 2.0f;
+
+    void Start ()
     {
         m_Rb = GetComponent<Rigidbody>();
         m_LastPosition = transform.position;
+        //StartCoroutine(DebugVibrationCoroutine());
     }
 	
     void Update ()
@@ -26,9 +35,21 @@ public class SaberBlade : MonoBehaviour {
     {
         Debug.Log(m_Direction.normalized);
         collider.gameObject.GetComponent<Rigidbody>().velocity = m_Direction.normalized * BladeStrength;
+        
+        hapticSignal.Execute(timeBeforeActivation, Duration, Frequency, Amplitude, SteamVR_Input_Sources.RightHand);
         if (collider.CompareTag("damage"))
         {
             
+        }
+    }
+
+    private IEnumerator DebugVibrationCoroutine()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(2);
+            hapticSignal.Execute(timeBeforeActivation, Duration, Frequency, Amplitude, SteamVR_Input_Sources.RightHand);
+
         }
     }
 }
