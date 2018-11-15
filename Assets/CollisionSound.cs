a60b51b3bc25bef8dde029ef8cc21699ace64493
rmexia <remondo86@hotmail.fr>
2018-11-15 12:45:14 +0100

    (feat):Assets

---
+++ Assets/CollisionSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+
+    public AudioSource AudioSource = null;
+    public AudioClip[] Clips = null;
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("bang");
+        AudioClip randomclip = Clips[Random.Range(0,3)];
+
+        AudioSource.clip = randomclip;
+        if (!AudioSource.isPlaying)
+        {
+            AudioSource.Play();
+        }
+    }
+}
